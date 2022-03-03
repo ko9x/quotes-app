559dcd6b3320a0b01f77c75396e0012e286ce9e4
Sean McGillis <slcidevice@yahoo.com>
2022-03-03 14:34:20 -0700

    Removed unused QuotesDetails page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import { Route, Switch } from "react-router-dom";
 import MainNavigation from "./components/layout/MainNavigation";
 import Quotes from "./pages/Quotes";
 import AddQuote from "./pages/AddQuote";
-import QuoteDetails from "./pages/QuoteDetails";
 
 function App() {
   return (
     <section>
       <MainNavigation></MainNavigation>
       <Switch>
-        <Route path="/quotes" component={Quotes} exact></Route>
+        <Route path="/quotes" component={Quotes} ></Route>
       </Switch>
       <Switch>
         <Route path="/add-quote" component={AddQuote} exact></Route>
-      </Switch>
-      <Switch>
-        <Route path="/quotes/:quoteID" component={QuoteDetails}></Route>
       </Switch>
     </section>
   );
--- src/pages/QuoteDetails.js
+++
@@ -1,9 +0,0 @@
-import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
-const QuoteDetails = () => {
-    return (
-        <HighlightedQuote author={'Some Author'} text={'some text'}/>
-    )
-};
-
-export default QuoteDetails;
